fdce4375fab951b18f3893224e840efe03ef97e4
vella-cyber <vella@staffjak-stik.ac.id>
2026-06-26 13:26:38 +0700

    menambahkan fitur file log request dan response

--- config/settings.py
+++ config/settings.py
@@ -121,3 +121,30 @@ STATIC_URL = 'static/'
 CORS_ALLOWED_ORIGINS = [
 "http://localhost:3000",
 ] 
+
+# Konfigurasi Otomatis File Log Request dan Response
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '{levelname} {asctime} {module} {message}',
+            'style': '{',
+        },
+    },
+    'handlers': {
+        'file': {
+            'level': 'INFO',
+            'class': 'logging.FileHandler',
+            'filename': 'request_response.log',  # Nama file log yang akan dibuat
+            'formatter': 'verbose',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['file'],
+            'level': 'INFO',
+            'propagate': True,
+        },
+    },
+}
